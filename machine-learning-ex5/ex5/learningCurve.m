b5a26451c74e35178558e3117beb7619162564d4
Chris Gomes <ugali_guy@hotmail.com>
2016-03-24 13:02:04 -0500

    Fix error on training and validation sets to learningCurve.m

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -55,12 +55,12 @@ error_val   = zeros(m, 1);
 
 for i = 1:m
 	theta = trainLinearReg(X(1:i, :), y(1:i), lambda)
-	error_train(i) = (1/(2*i))*sum((X(1:i, :)*theta - y(1:i)).^2);
-	error_val(i) = (1/(2*m))*sum((Xval(1:m, :)*theta - y(1:m)).^2);
+	[error_train(i), grad] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+	[error_val(i), grad] = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
 % -------------------------------------------------------------
-
+	
 % =========================================================================
 
 end
